156bab88bf2181362f9207172825734455320890
ГерценЕ <@N206-T>
2021-04-06 21:41:38 +0600

    Добавьте файлы проекта.

---
+++ CardGame/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    enum TypeCard
+    {
+        Chervi, Bubi, Piki, Krestya
+    }
+    
+    class Card
+    {
+        public TypeCard mast { get; set; }
+        public int cardNumber { get; set; }
+    }
+}
---
+++ CardGame/Game.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    class Game
+    {
+        public List<Player> players { get; set; }
+        public Stack<Card> cards { get; set; }
+        Random rnd = new Random();
+        public void createPlayer()
+        {
+            for (int i = 0; i < rnd.Next(2, 4); i++)
+            {
+                Player player = new Player();
+                player.Name = "Player " + i;
+                players.Add(player);
+            }
+        }
+        public void createCards()
+        {
+            foreach (TypeCard item in (TypeCard[])Enum.GetValues(typeof(TypeCard)))
+            {
+                for (int i = 6; i <= 14; i++)
+                {
+                    Card card = new Card();
+                    card.mast = item;
+                    card.cardNumber = i;
+                    cards.Push(card);
+                }
+            }
+        }
+        public void distribution()
+        {
+            while (cards.Count>0)
+            {
+                for (int i = 0; i < players.Count; i++)
+                {
+                    players[i].cards.Add(cards.Pop());
+                }
+            }
+        }
+    }
+}
